98fffda0a9b3a58aa44bd443d35f9557f35acea0
Vlggomes <74120529+Vlggomes@users.noreply.github.com>
2021-02-01 01:36:31 -0800

    message

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// add function
+function addition(num1,num2){
+    var result = num1 + num2;
+    document.write("sum = " + result + "<br>");
+}
+addition(29,38)
+addition(2560,1523)
+// sub function
+function subtraction(num1,num2){
+    var result = num1 - num2;
+    document.write("sub =" + result + "<br>")
+}
+subtraction(1895,286)
+subtraction(42186,4582)
+// multiply function
+function multiplication(num1,num2){
+    var result = num1 * num2;
+    document.write("multiply =" + result + "<br>");
+}
+multiplication(4251,8547)
+multiplication(619,7528)
+// division function
+function division(num1,num2){
+    var result = num1 / num2;
+    document.write("divide =" + result + "<br>");
+}
+division(4219,85)
+division(429,428)
